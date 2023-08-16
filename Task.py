2a8536cca78dba52702d7ede3db3ca064284c2cc
Aryan39232 <20bcs097@iiitdwd.ac.in>
2023-08-17 01:31:14 +0530

    Chatbot

---
+++ Task.py
@@ -0,0 +1,43 @@
+import datetime  # Make sure to import the datetime module
+
+def Say(text):
+    print(text)  # Replace this with actual code to speak the text
+
+def Time():
+    time = datetime.datetime.now().strftime("%H:%M")
+    Say(time)
+
+def Date():  # Correct function name to "Date" instead of "Dime"
+    date = datetime.date.today().strftime("%Y-%m-%d")
+    Say(date)
+
+def Day():
+    day = datetime.datetime.now().strftime("%A")
+    Say(day)
+
+
+def NonInputExecution(query):
+    query = str(query)
+
+    if "time" in query:
+        Time()
+    elif "date" in query:  # Change "data" to "date"
+        Date()
+    elif "day" in query:
+        Day()
+
+# Test your functions
+
+
+def InputExecution(tag , query):
+    if "wikipedia" in tag:
+        name = str(query).replace("" ,"")
+        import wikipedia
+        result = wikipedia.summary(name)
+        Say(result)
+
+    elif "google" in tag:
+        query = str(query).replace("google" , "").replace("search" ,"")
+        query = query.replace
+        import pywhatkit
+        pywhatkit.search(query)
