3faf5629d259f65e7ab0a9f0839d20bfad9ef975
Aryan39232 <20bcs097@iiitdwd.ac.in>
2023-08-17 02:11:37 +0530

    Final_code

--- Task.py
+++ Task.py
@@ -1,14 +1,13 @@
 import datetime  # Make sure to import the datetime module
+from Speak import Say
 
-def Say(text):
-    print(text)  # Replace this with actual code to speak the text
 
 def Time():
     time = datetime.datetime.now().strftime("%H:%M")
     Say(time)
 
 def Date():  # Correct function name to "Date" instead of "Dime"
-    date = datetime.date.today().strftime("%Y-%m-%d")
+    date = datetime.date.today().today()
     Say(date)
 
 def Day():
